afd98d66f1b9b12d815939b071e239e5668669bd
WissamDouskary <wissamdouskary99@gmail.com>
2025-01-19 15:07:04 +0100

    configure sweetalerts on banned or approved users

--- Handling/userHandl.php
+++ Handling/userHandl.php
@@ -1,10 +1,24 @@
 <?php 
+session_start();
 require_once '../classes/admin.php';
 
 if(isset($_POST['user_id']) && isset($_POST['action'])){
     Admin::changeEnseignant($_POST['user_id'], $_POST['action']);
-    header('Location: ../pages/adminDashboard.php');
-    exit();
+    if($_POST['action'] == 'active'){
+        $_SESSION['message'] = [
+            'type' => 'success',
+            'text' => 'you have approve user where user id = ' . $_POST['user_id']
+        ];
+        header('Location: ../pages/adminDashboard.php');
+        exit();
+    }else if ($_POST['action'] == 'suspended'){
+        $_SESSION['message'] = [
+            'type' => 'success',
+            'text' => 'you have banned user where user id = ' . $_POST['user_id']
+        ];
+        header('Location: ../pages/adminDashboard.php');
+        exit();
+    }
 }
 
 ?>
